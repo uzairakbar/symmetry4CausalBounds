19ca8d66711201ed00aaf2766e02028893a1907a
Uzair Akbar <uzairakbar025@gmail.com>
2026-08-30 07:01:33 -0400

    init site from academic-paper theme

---
+++ scripts/katex.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const katex = require('katex');
+
+function renderFile(p) {
+    const t = fs.readFileSync(p, {encoding: 'utf8', flag: 'r'});
+    const u = t.replaceAll(/<code>\$([^\$]*)\$<\/code>/g, (_, m) => katex.renderToString(m.replaceAll('&lt;','<').replaceAll('&gt;','>')));
+    const v = u.replaceAll(/<pre[^>]*><code[^>]*>(?:<span[^>]*>)*\s*\$\$([^\$]*)\$\$\s*(?:<\/span>)*<\/code><\/pre>/g, (_, m) => '<p>' + katex.renderToString(m.replaceAll(/<\/?span[^>]*>/g, '').replaceAll('&lt;','<').replaceAll('&gt;','>'), {displayMode: true}) + '</p>');
+    fs.writeFileSync(p, v);
+}
+
+function renderDirectory(d) {
+    fs.readdirSync(d).forEach(f => {
+        const p = path.join(d, f);
+        if (path.extname(f) === '.html') {
+            renderFile(p);
+        } else if (fs.statSync(p).isDirectory()) {
+            renderDirectory(p);
+        }
+    });
+}
+
+renderDirectory('public');
